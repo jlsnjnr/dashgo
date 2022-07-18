78488ecf3dcab795f3a406aeded82af20858f642
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-07-18 18:55:41 -0300

    separando componentes

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,17 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex
+      align="center"
+    >
+      <Box mr="4" textAlign="right">
+        <Text>Júnior Andrade</Text>
+        <Text color="gray.300" fontSize="small">jnr0444@gmail.com</Text>
+      </Box>
+
+      <Avatar size="md" name="Junior Andrade" src="https://media-exp2.licdn.com/dms/image/C4D03AQErODt_PGaOYA/profile-displayphoto-shrink_800_800/0/1655159393885?e=1663200000&v=beta&t=vFZHo0RPl6c753ap1g1qixyoon4ju_uLuCTayyhiLZs" />
+
+    </Flex>
+  );
+}
